7c5aca755fd24314c191915b5fd0de7bfb9d35c9
Wanda Gleason <oglissane@gmail.com>
2021-03-29 00:32:02 -0400

    Added post route.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const postRoutes = require('./post-routes');
 
 // router.use("/users", userRoutes);
 router.use(userRoutes);
+module.exports = router;
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
+
 module.exports = router;
