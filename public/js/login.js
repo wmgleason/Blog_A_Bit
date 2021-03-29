e02725b01832fa803887126d7055351f4f3a7f3b
Wanda Gleason <oglissane@gmail.com>
2021-03-28 22:46:25 -0400

    Added function to handle signup.

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,57 @@
-const loginFormHandler = async (event) => {
-  // Stop the browser from submitting the form so we can do so with JavaScript
+async function signupHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    // check the response status
+    if (response.ok) {
+      document.location.replace('/dashboard');
+      console.log('Congrats - your new account has been created.');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+const loginHandler = async (event) => {
+  // This should stop the browser from automatically submitting the login form
   event.preventDefault();
 
   // Gather the data from the form elements on the page
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (email && password) {
     // Send the e-mail and password to the server
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace("/dashboard");
     } else {
-      alert('Log in has failed.');
+      alert("Log in has failed. Your username or password must be incorrect.");
     }
   }
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginHandler);
+
+  
