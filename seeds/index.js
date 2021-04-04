7b10302706090e50f15ab309225854ae414ae52c
Wanda Gleason <oglissane@gmail.com>
2021-04-04 17:22:34 -0400

    Added seedDatabase.

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,12 @@
-const seedUsers = require('./userSeeds');
-const seedPosts = require('./postSeeds');
-const seedComments = require('./commentSeeds');
-
-const sequelize = require('../config/connection');
-
-const seedAll = async() => {
-    await sequelize.sync({ force: true });
-    await seedUsers();
-    await seedPosts();
-    await seedComments();
-    process.exit(0);
+const sequelize = require("../config/connection");
+const { User } = require("../models");
+const userData = require("./userData.json");
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit(0);
 };
-
-seedAll();
+seedDatabase();
