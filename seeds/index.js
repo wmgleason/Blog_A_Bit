5f9a0b328193f79e93b76d0081d0fd0d9e769eaf
Wanda Gleason <oglissane@gmail.com>
2021-04-04 17:18:29 -0400

    Added index.js to seeds.

---
+++ seeds/index.js
@@ -0,0 +1,15 @@
+const seedUsers = require('./userSeeds');
+const seedPosts = require('./postSeeds');
+const seedComments = require('./commentSeeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async() => {
+    await sequelize.sync({ force: true });
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+    process.exit(0);
+};
+
+seedAll();
