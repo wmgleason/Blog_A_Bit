b7942874a1b3a64c997ba79a7ba11b243d9dd38e
Wanda Gleason <oglissane@gmail.com>
2021-04-04 16:41:25 -0400

    Added models relationships.

--- models/index.js
+++ models/index.js
@@ -1,64 +1,37 @@
 "use strict";
-const { Model, DataTypes } = require('sequelize');
-// const Post = require("./Post");
+// const { Model, DataTypes } = require('sequelize');
+const Post = require("./Post");
+const Comment = require('./Comment');
+const User = require('./User');
 const sequelize = require('../config/connection');
-class Post extends Model {}
-Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id'
-      }
-    }
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post'
-  }
-);
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))
-//     (sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
+// models relationships
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
 
-// User/Post associations
-// User.hasMany(Post, {
-//   foreignKey: 'user_id'
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
 
-// Post.belongsTo(User, {
-//   foreignKey: 'user_id',
-// });
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: "cascade"
+});
 
-module.exports = Post;
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: "cascade"
+})
+//need this for all these models
+module.exports = { User, Post, Comment };
